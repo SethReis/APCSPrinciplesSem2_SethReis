94da2954055abf99c5abc6ee9d56991bd0063382
Seth Reis <795091@seq.org>
2019-01-14 08:48:16 -0800

    added var checks
    just remember to uncomment to make it work

--- Labs/Lab 109/script.js
+++ Labs/Lab 109/script.js
@@ -14,50 +14,66 @@ function init(){
 
 function getJSON() {
   var array = [];
+  var compares = 0;
+  var swaps = 0;
+  var time = new Date();
+
   $.getJSON("data.json", function(json) {
     for (var i = 0; i < json.crimes.length; i++){
       array[i] = json.crimes[i];
     }
-    /* mySort
+    /*mySort
     var frontCount = 0;
     var backCount = array.length-1;
+    var now1 = time.getTime();
 
     for(var i = 0; i < array.length/2; i++){
       for(var j = frontCount; j < backCount; j++){
+        compares++;
         if(array[j].stopandsearch.length > array[j+1].stopandsearch.length){
           var temp = array[j];
           array[j] = array[j+1];
           array[j+1] = temp;
+          swaps++;
         }
       }
       backCount--;
       for(var k = backCount; k > frontCount; k--){
+        compares++;
         if(array[k].stopandsearch.length < array[k-1].stopandsearch.length){
           var temp = array[k];
           array[k] = array[k-1];
           array[k-1] = temp;
+          swaps++;
         }
       }
       frontCount++;
     }
+    var now2 = time.getTime();
     */
 
-    /* bubbleSort
+    /*bubbleSort
+    var now1 = time.getTime();
     for (var i = array.length-1; i > 0; i--){
       for(var j = 0; j < i; j++){
+        compares++;
         if (array[j].stopandsearch.length > array[j+1].stopandsearch.length){
           var temp = array[j].stopandsearch.length;
           array[j].stopandsearch.length = array[j+1].stopandsearch.length;
           array[j+1].stopandsearch.length = temp;
+          swaps++;
         }
       }
     }
+    var now2 = time.getTime();
     */
 
-    /* selectionSort
+    /*selectionSort
+    var now1 = time.getTime();
     for(var i = 0; i < array.length-1; i++){
       var index = i;
       for(var j = i+1; j < array.length; j++){
+        compares++;
         if(array[j].stopandsearch.length < array[index].stopandsearch.length){
           index = j;
         }
@@ -65,20 +81,30 @@ function getJSON() {
       var temp = array[index].stopandsearch.length;
       array[index].stopandsearch.length = array[i].stopandsearch.length;
       array[i].stopandsearch.length = temp;
+      swaps++;
     }
+    var now2 = time.getTime();
     */
 
-    /* insertSort
+    /*insertSort
+    var now1 = time.getTime();
     for(var i = 1; i < array.length; i++){
       for(var j = i; j > 0; j--){
+        compares++;
         if(array[j].stopandsearch.length < array[j-1].stopandsearch.length){
           var temp = array[j].stopandsearch.length;
           array[j].stopandsearch.length = array[j-1].stopandsearch.length;
           array[j-1].stopandsearch.length = temp;
+          swaps++;
         }
       }
     }
+    var now2 = time.getTime();
     */
+
+    console.log("Time taken to complete: " + (now2 - now1) + " ms");
+    console.log("Number of comparisons made: " + compares + " comparisons");
+    console.log("Number of swaps made: " + swaps + " swaps");
     console.log(array);
   });
 }
