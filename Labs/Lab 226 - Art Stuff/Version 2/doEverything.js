67db2f9373f58e30c506c68fd3b691144866062e
Seth Reis <795091@seq.org>
2019-03-06 08:39:58 -0800

    art labs done

---
+++ Labs/Lab 226 - Art Stuff/Version 2/doEverything.js
@@ -0,0 +1,84 @@
+window.onload = init; // Wait for the page to load before we begin animation
+var canvas;
+var ctx;// This is a better name for a global variable
+var parts = [];
+var k = 0;
+var maxLine = 10;
+
+function init(){
+  //get the canvas
+  canvas = document.getElementById('cnv');
+  // Set the dimensions of the canvas
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  canvas.style.border = 'solid black 0px';
+  //var color = Math.floor((Math.random()*2)+1);
+  //canvas.style.backgroundColor = '';
+  canvas.style.backgroundColor = 'rgba(' + Math.floor(Math.random()*255) + ',' + Math.floor(Math.random()*255) + ',' + Math.floor(Math.random()*255) + ',' + 1 + ')';
+  // get the context
+  ctx = canvas.getContext('2d'); // This is the context
+  for (var i = 0; i < 20; i++){
+    parts.push(new Part(new JSVector(canvas.width/2, canvas.height/2)));
+  }
+  mover = new Mover();
+  orb = new Orb();
+  snake = new Snake();
+  animate(); // Call to your animate function
+}
+// To do::
+//  1. Declare and init variables x, y, dx, dy, radius;
+function animate(){
+  requestAnimationFrame(animate);
+  // ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+  ctx.rect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = canvas.style.backgroundColor;
+  ctx.fill();
+  ctx.strokeStyle = canvas.style.backgroundColor;
+  ctx.stroke();
+  ctx.closePath();
+
+  k++;
+  // Looping through backwards to delete
+  for (var i = parts.length-1; i >= 0; i--) {
+    if (k === 10){
+      parts.push(new Part(new JSVector(canvas.width/2, canvas.height/2)));
+      k = 0;
+    }
+    var part = parts[i];
+    part.update();
+    part.render();
+    if (part.isDead()) {
+      //remove the particle
+      parts.splice(i, 1);
+    }
+    var distance = snake.snak.distance(part.position)
+    if (distance <= 200){
+      var f = JSVector.subGetNew(part.position, mover.loc);
+      f.normalize();
+      f.mult(.3);
+      mover.applyForce(f);
+    }
+    for (var j = 0; j < parts.length; j++){
+      if (parts[j] != part){
+        part.makeConnections(parts[j]);
+      }
+    }
+  }
+  mover.update();
+  mover.render();
+  for(var j = 0; j < parts.length; j++){
+    var part= parts[j];
+    var distance = part.position.distance(snake.snak)
+    if (distance <= 200){
+      var f = JSVector.subGetNew(part.position, JSVector.addGetNew(mover.loc, orb.loc));
+      f.normalize();
+      f.mult(.3);
+      part.applyForce(f);
+    }
+  }
+  orb.update();
+  orb.render();
+  snake.update();
+  snake.render();
+}
---
+++ Labs/Lab 226 - Art Stuff/Version 2/particles.js
@@ -0,0 +1,54 @@
+function Part(position){
+  this.acceleration = new JSVector(0, 0);
+  this.velocity = new JSVector(Math.random()*10-5, Math.random()*10-5);
+  this.position = position.copy();
+  this.lifespan = 1000.0;
+  this.rad = Math.random()*15+10;
+  this.star = 'rgba(' + Math.floor(Math.random()*255) + ',' + Math.floor(Math.random()*255) + ',' + Math.floor(Math.random()*255) + ',';
+}
+
+Part.prototype.update = function(){
+  this.velocity.add(this.acceleration);
+  this.position.add(this.velocity);
+  this.velocity.limit(3);
+  this.lifespan -= 2;
+  if(this.position.x + this.rad >= canvas.width || this.position.x - this.rad <= 0){
+    this.position.x = canvas.width-this.position.x;
+  }
+  if(this.position.y + this.rad >= canvas.height || this.position.y - this.rad <= 0){
+    this.position.y= canvas.height-this.position.y;
+  }
+}
+
+Part.prototype.render = function(){
+  // ctx.beginPath();
+  // ctx.ellipse(this.position.x, this.position.y, this.rad, this.rad, 0, 0, 2*Math.PI);
+  // this.c = this.star + this.lifespan/1000.0 + ')';
+  // ctx.fillStyle = this.c;
+  // ctx.fill();
+  // ctx.strokeStyle = this.c;
+  // ctx.stroke();
+}
+
+Part.prototype.makeConnections = function(otherBall){
+  for (var r = 0; r < maxLine; r++){
+    ctx.beginPath();
+    ctx.moveTo((this.position.x),(this.position.y));
+    ctx.lineTo((otherBall.position.x),(otherBall.position.y));
+    //ctx.strokeStyle = 'rgba(0, 0, 0, 255)';
+    ctx.strokeStyle = this.star + this.lifespan/1000.0 + ')';
+    ctx.stroke();
+  }
+}
+
+Part.prototype.isDead = function(){
+  if (this.lifespan < 0.0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+Part.prototype.applyForce = function(f){
+  this.acceleration.add(f);
+}
